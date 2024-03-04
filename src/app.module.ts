e67e2be1b2ce1fa9584ba89f299103e34a54edd8
VaSeala <vaseala.com@gmail.com>
2024-03-04 17:35:57 +0800

    complted article module and tag module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticleModule } from './article/article.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { resolve } from 'path';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: resolve(__dirname, '../db/app.sqlite'),
+      autoLoadEntities: true,
+      // entities: [Article],
+      synchronize: true,
+    }),
+    ArticleModule,
+    TagModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
